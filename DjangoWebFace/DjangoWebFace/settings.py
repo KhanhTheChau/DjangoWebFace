106fc4e13f035422f7994a665e226a81514c31d8
CHAUTHEKHANH\Admin <khanhchau5678@gmail.com>
2023-12-15 23:29:20 +0700

    update

--- DjangoWebFace/DjangoWebFace/settings.py
+++ DjangoWebFace/DjangoWebFace/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-xjc6e70*-!4o&u8sqgejho7xjt=j49mb3#j2e!%=0j9(bqs*5_
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['vercel.app']
 
 
 # Application definition
